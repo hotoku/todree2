22e7b7c2c26e430deb610a77f522df8f8440507b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-04-14 18:41:34 +0900

    [tiny]

--- fe/src/api.ts
+++ fe/src/api.ts
@@ -42,7 +42,6 @@ export async function createItem(item: ItemCreate): Promise<ItemContent> {
     body: JSON.stringify(item),
   });
   const data: SuccessResponse = await res.json();
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   return data;
 }
 
