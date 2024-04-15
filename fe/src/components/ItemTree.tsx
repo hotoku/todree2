09a2c33f92fbdb2f6db97df80cdcd9272c51d5fd
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-04-16 07:10:49 +0900

    [upd] show tree in components

--- fe/src/components/ItemTree.tsx
+++ fe/src/components/ItemTree.tsx
@@ -1,7 +1,34 @@
 import { useAtomValue } from "jotai";
 import { treeAtom } from "../atoms";
+import { ValueNode } from "../tree/tree";
 
 export function ItemTree(): JSX.Element {
   const tree = useAtomValue(treeAtom).getOrThrow();
-  return <span>{JSON.stringify(tree)}</span>;
+  const root = tree.root;
+  const children = root.children;
+  return (
+    <ul>
+      {children.map((c) => (
+        <ItemSubtree root={c} />
+      ))}
+    </ul>
+  );
+}
+
+type ItemSubtreeProps = {
+  root: ValueNode;
+};
+
+function ItemSubtree({ root }: ItemSubtreeProps): JSX.Element {
+  const children = root.children;
+  return (
+    <li key={root.id}>
+      {root.content}
+      <ul>
+        {children.map((c) => (
+          <ItemSubtree root={c} />
+        ))}
+      </ul>
+    </li>
+  );
 }
