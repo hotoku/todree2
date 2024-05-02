0e2656f7fd93b98e4a78c53d56c89a6128082461
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2024-05-03 06:26:05 +0900

    [upd] delete, create works

--- fe/src/api.ts
+++ fe/src/api.ts
@@ -2,65 +2,73 @@ import { components, paths } from "./schema";
 
 export type Item = components["schemas"]["Item"];
 
-const db: {
-  [key: number]: { id: number; content: string; parent: number | null };
-} = {
-  1: { id: 1, content: "one", parent: null },
-  2: { id: 2, content: "two", parent: 1 },
-  3: { id: 3, content: "three", parent: 1 },
-  4: { id: 4, content: "four", parent: 2 },
-};
-
 export async function loadRoot(): Promise<Item[]> {
   type Response200 =
     paths["/api/items"]["get"]["responses"]["200"]["content"]["application/json"];
 
   const res = await fetch("/api/items");
+  if (!res.ok) {
+    throw new Error("Failed to add item");
+  }
   const data: Response200 = await res.json();
   return data;
-}
-
-export function sleep(n: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, n * 1000));
 }
 
 export async function loadChildren(parent: number): Promise<Item[]> {
   type Response200 =
     paths["/api/items/{item_id}/children"]["get"]["responses"]["200"]["content"]["application/json"];
   const res = await fetch(`/api/items/${parent}/children`);
+  if (!res.ok) {
+    throw new Error("Failed to add item");
+  }
   const data: Response200 = await res.json();
   return data;
 }
 
 export async function saveContent(id: number, v: string): Promise<string> {
+  type Body =
+    paths["/api/items/{item_id}"]["put"]["requestBody"]["content"]["application/json"];
   type Response200 =
     paths["/api/items/{item_id}"]["put"]["responses"]["200"]["content"]["application/json"];
+  const body: Body = { content: v };
   const res = await fetch(`/api/items/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ content: v }),
+    body: JSON.stringify(body),
   });
+  if (!res.ok) {
+    throw new Error("Failed to add item");
+  }
   const data: Response200 = await res.json();
   return data.content;
 }
 
-export function addItem(parentId: number | null): Promise<Item> {
-  const adding = sleep(1).then(() => {
-    const id = Math.max(...Object.keys(db).map(Number)) + 1;
-    const item = { id, content: "new item", parent: parentId };
-    db[id] = item;
-    return item;
+export async function addItem(parentId: number | null): Promise<Item> {
+  type Body =
+    paths["/api/items"]["post"]["requestBody"]["content"]["application/json"];
+  const body: Body = { parent_id: parentId, content: "" };
+  type Response200 =
+    paths["/api/items"]["post"]["responses"]["200"]["content"]["application/json"];
+  const res = await fetch(`/api/items`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
   });
-  return adding;
+  if (!res.ok) {
+    throw new Error("Failed to add item");
+  }
+  const data: Response200 = await res.json();
+  console.log(data);
+  return data;
 }
 
-export function deleteItem(id: number): Promise<void> {
-  const children = Object.values(db).filter((item) => item.parent === id);
-  const deleting = sleep(1).then(() => {
-    if (children.length > 0) {
-      throw new Error("Cannot delete node with children");
-    }
-    delete db[id];
+export async function deleteItem(id: number): Promise<void> {
+  const res = await fetch(`/api/items/${id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
   });
-  return deleting;
+  if (!res.ok) {
+    throw new Error("Failed to add item");
+  }
+  return;
 }
